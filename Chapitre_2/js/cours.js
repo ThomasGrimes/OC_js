724c0cdd8627eda027c8c375304d73c1b04f13c2
Thomas Renaud <trenaud@stva.com>
2018-04-06 18:27:27 +0200

    TVA a faire

---
+++ Chapitre_2/js/cours.js
@@ -0,0 +1,19 @@
+/*var prenom;
+var nom;
+
+prenom = prompt("Quel est votre prénom ?");
+nom = prompt("Quel est votre nom ?");
+
+alert("Bonjour " + prenom + " " + nom);
+*/
+
+var a = 2;
+a = a - 1;
+a++;
+var b = 8;
+b += 2;
+var c = a + b * b;
+var d = a * b + b;
+var e = a * (b + b);
+var f = a * b / a;
+var g = b / a * a;
