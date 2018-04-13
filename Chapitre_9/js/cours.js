86e7308c57927eeb15de6fa35975551720326925
Thomas Renaud <trenaud@stva.com>
2018-04-13 18:19:34 +0200

    Modif Chap 9

---
+++ Chapitre_9/js/cours.js
@@ -0,0 +1,27 @@
+// ################################### Mousquetaires ###########################################
+/*
+console.log("Voici les 3 mousquetaires :");
+
+var mousquetaires = ["Aramis", "Portos", "Athos"];
+
+for (var i = 0; i <= mousquetaires.length-1; i++) {
+    console.log(mousquetaires[i]);
+}
+
+mousquetaires.push("D'Artagnan");
+
+console.log("A présent, ils sont quatre !");
+
+mousquetaires.forEach(function (perso) {
+    console.log(perso);
+});
+*/
+// ################################### Somme ###########################################
+
+var valeurs = [11, 3, 7, 2, 9, 10];
+
+valeurs.forEach(function(nb){
+   var chiffre += nb ;
+});
+
+console.log(chiffre);
